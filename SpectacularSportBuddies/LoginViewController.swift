fcae36a09fa890f48999cffcbf27c4743a6fab92
wash_u_adela <adela.xy.gao@gmail.com>
2016-07-23 14:42:28 -0700

    first commit with facebook login

---
+++ SpectacularSportBuddies/LoginViewController.swift
@@ -0,0 +1,32 @@
+//
+//  LoginViewController.swift
+//  SpectacularSportBuddies
+//
+//  Created by Adela Gao on 7/23/16.
+//  Copyright © 2016 Adela Gao. All rights reserved.
+//
+
+import UIKit
+import FBSDKLoginKit
+
+class LoginViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    override func viewWillAppear(animated: Bool) {
+        super.viewWillAppear(animated)
+        addFBLoginButton()
+    }
+    
+    func addFBLoginButton() {
+        let fbLoginButton = FBSDKLoginButton()
+        fbLoginButton.center = view.center
+        view.addSubview(fbLoginButton)
+        
+    }
+
+}
